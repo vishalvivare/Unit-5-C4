47542f748333feba07c8a1cfe34bcc2006f4f4f2
Vishal Vivare <96101110+vishalvivare@users.noreply.github.com>
2022-05-02 17:01:44 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { Logout } from "./components/Logout";
+import { NewOrder } from "./components/NewOrder";
+import { Orders } from "./components/Orders";
+import { ProtectedRoute } from "./components/ProtextedRoute";
+import{Link} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Link className="nav-home" to="/">
+          Home
+        </Link>
+        {/* Show either login or logout below */}
+        <Link className="nav-logout" to="/logout">
+          Logout
+        </Link>
+        <Link className="nav-login" to="/login">
+          Login
+        </Link>
+      </div>
+
+      <Routes>
+     <Route path="/" element={<Home/>}></Route>
+     <Route path="/login" element={< Login/>}></Route>
+     <Route path="/logout" element={<Logout />}></Route>
+     <Route path="/newOrder" element={<NewOrder/>}></Route>
+     <Route path="/orders" element={<Orders/>}></Route>
+     <Route path="/protected" element={<ProtectedRoute/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
